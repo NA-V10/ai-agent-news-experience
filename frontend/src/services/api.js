cfb6413adc9facadcd584e2c6abc34aadf5450d9
NA-V10 <naveensbsf123@gmail.com>
2026-03-22 08:15:23 +0530

    Api added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8000";
+const API = "https://ai-agent-news-experience.onrender.com";
 
 export const getNews = async (type) => {
   const res = await axios.get(`${API}/news?user_type=${type}`);
